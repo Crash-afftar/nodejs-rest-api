d0ce8036aaa03f29af731da69f68da84310dfdd4
Crash-afftar <sashakhymenko88@gmail.com>
2023-07-19 23:12:10 +0300

    1907

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const gravatar = require("gravatar");
 const path = require("path");
 const fs = require("fs/promises");
+const jimp = require("jimp");
 
 const { User } = require("../models/user");
 
@@ -83,13 +84,23 @@ const updateAvatar = async (req, res) => {
   const { path: tempUpload, originalname } = req.file;
   const filename = `${_id}_${originalname}`;
   const resultUpload = path.join(avatarsDir, filename);
-  await fs.rename(tempUpload, resultUpload);
-  const avatarURL = path.join("avatars", filename);
-  await User.findByIdAndUpdate(_id, { avatarURL });
 
-  res.json({
-    avatarURL,
-  });
+  try {
+    const avatar = await jimp.read(tempUpload);
+
+    await avatar.resize(250, 250);
+    await avatar.write(resultUpload);
+
+    await fs.unlink(tempUpload);
+
+    const avatarURL = path.join("avatars", filename);
+    await User.findByIdAndUpdate(_id, { avatarURL });
+    res.json({
+      avatarURL,
+    });
+  } catch (error) {
+    throw new HttpError(500, "Email or password incorrect");
+  }
 };
 
 module.exports = {
